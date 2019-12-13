0a9b35188a5225e81b6afc4c831509156466d5d9
Philip Maaß <PhilipMaasz@aol.com>
2019-12-13 15:05:43 +0100

    implements Media entity

--- src/Entity/Media.php
+++ src/Entity/Media.php
@@ -1,10 +1,60 @@
 <?php
 
-
 namespace App\Entity;
 
-
+/**
+ * Class Media
+ *
+ * @package App\Entity
+ *
+ * @ORM\Entity()
+ * @ORM\Table(name="media")
+ */
 class Media
 {
+    /**
+     * @var string
+     * @ORM\Id()
+     * @ORM\GeneratedValue(strategy="UUID")
+     * @ORM\Column(type="string", length=190)
+     */
+    private $uuid;
 
+    /**
+     * @var Entry
+     * @ORM\OneToOne(targetEntity="Entry")
+     */
+    private $entry;
+
+    /**
+     * @return string
+     */
+    public function getUuid(): string
+    {
+        return $this->uuid;
+    }
+
+    /**
+     * @param string $uuid
+     */
+    public function setUuid(string $uuid): void
+    {
+        $this->uuid = $uuid;
+    }
+
+    /**
+     * @return Entry
+     */
+    public function getEntry(): Entry
+    {
+        return $this->entry;
+    }
+
+    /**
+     * @param Entry $entry
+     */
+    public function setEntry(Entry $entry): void
+    {
+        $this->entry = $entry;
+    }
 }
