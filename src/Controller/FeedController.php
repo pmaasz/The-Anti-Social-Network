08ddc41ff151a5b84ecc469a3a8c55310ae11e34
ebalon09 <Florian.Stein99@t-online.de>
2019-12-14 12:14:28 +0100

    .

--- src/Controller/FeedController.php
+++ src/Controller/FeedController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Entry;
+use App\Forms\EntryType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Security\Core\Security;
 
 /**
  * Class FeedController
@@ -14,14 +17,43 @@ use Symfony\Component\HttpFoundation\Response;
 class FeedController extends AbstractController
 {
     /**
-     * @return Response
+     * @var Security
      */
-    public function indexAction()
+    private $security;
+
+    public function __construct(Security $security)
     {
+        $this->security = $security;
+    }
+
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|Response
+     * @throws \Exception
+     */
+    public function indexAction(Request $request)
+    {
+        $entry = new Entry();
         $entries = $this->getDoctrine()->getRepository(Entry::class)->findAll();
+        $form = $this->createForm(EntryType::class, $entry);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $entry->setAuthor($this->security->getUser());
+
+            $this->getDoctrine()->getManager()->persist($entry);
+            $this->getDoctrine()->getManager()->flush();
+
+            $this->addFlash('success', 'Willkommen bei Roundabout. Sie haben sich erfolgreich registriert.');
+
+            return $this->redirectToRoute('feed');
+        }
 
         return $this->render('Feed/feed.html.twig', [
-            'entries' => $entries
+            'entries' => $entries,
+            'form' => $form->createView()
         ]);
     }
 
