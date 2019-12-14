c512de421f26ca562f6416d8b5abd8721e9b2757
Philip Maass <pmaass@databay.de>
2019-12-14 13:52:15 +0100

    no more landingpage

--- src/Controller/FeedController.php
+++ src/Controller/FeedController.php
@@ -77,7 +77,7 @@ class FeedController extends AbstractController
             $entityManager->flush();
 
 
-            return $this->redirectToRoute('feed');
+            return $this->redirectToRoute('index');
         }
 
         return $this->render('feed', [
